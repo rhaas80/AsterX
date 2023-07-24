390d9899495aeb8f87bb9debae099c038582635a
Jay Kalinani <jaykalinani@gmail.com>
2023-07-24 14:12:38 -0400

    AsterX: Bugfix in defining tabulated EOS object

--- AsterX/src/con2prim.cxx
+++ AsterX/src/con2prim.cxx
@@ -240,7 +240,8 @@ extern "C" void AsterX_Con2Prim(CCTK_ARGUMENTS) {
     CCTK_REAL rmd_p = pow(poly_k, -n);  // Polytropic density scale
 
     const eos_polytrope eos_cold(n, rmd_p, rho_max);
-    const eos_tabulated3d eos_th(EOSTable_filename);
+    const string filename = EOSTable_filename;
+    const eos_tabulated3d eos_th(filename, read_EOSTable_parallel);
 
     AsterX_Con2Prim_typeEoS(CCTK_PASS_CTOC, eos_cold, eos_th);
     break;
