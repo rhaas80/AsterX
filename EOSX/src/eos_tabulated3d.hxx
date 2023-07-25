2af0493d1115c6a0d409f202baf25f404409b69d
Lorenzo Ennoggi <le8016@rit.edu>
2023-07-25 10:57:00 -0500

    EOSX: some work for the tabulated EOS

--- EOSX/src/eos_tabulated3d.hxx
+++ EOSX/src/eos_tabulated3d.hxx
@@ -13,39 +13,13 @@ namespace EOSX {
 
 class eos_tabulated3d : public eos {
 public:
-  CCTK_REAL gamma;
+  CCTK_REAL gamma;  // FIXME: get rid of this
   range rgeps;
 
-  // constructor
-  CCTK_HOST CCTK_ATTRIBUTE_ALWAYS_INLINE inline eos_tabulated3d(
-      const string &filename, const bool &read_EOSTable_parallel, const range &rgeps_,
-    const range &rgrho_, const range &rgye_) {
-    auto fapl_id = H5Pcreate(H5P_FILE_ACCESS);
-    assert(fapl_id >= 0);
-    hid_t file_id = 0;
-    int mpi_rank_id;
-    CHECK_ERROR(MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank_id));
-
-    if(read_EOSTable_parallel){
-      // Open the input file collectively
-      CHECK_ERROR(H5Pset_fapl_mpio(fapl_id, MPI_COMM_WORLD, MPI_INFO_NULL));
-      CHECK_ERROR(H5Pset_all_coll_metadata_ops(fapl_id, true));
-      file_id = H5Fopen(filename.c_str(), H5F_ACC_RDONLY, fapl_id);
-    } 
-    else {
-      fapl_id = H5P_DEFAULT;
-
-      if (!mpi_rank_id) {
-        file_id = H5Fopen(filename.c_str(), H5F_ACC_RDONLY, fapl_id);
-      }
-    }
-  
-    assert(file_id >= 0);
-    CHECK_ERROR(H5Pclose(fapl_id));
-    if(read_EOSTable_parallel || !mpi_rank_id) {
-      CHECK_ERROR(H5Fclose(file_id));
-    }
-
+  // Constructor
+  CCTK_HOST CCTK_DEVICE CCTK_ATTRIBUTE_ALWAYS_INLINE inline  eos_tabulated3d(
+    const range &rgeps_, const range &rgrho_, const range &rgye_)
+  {
     set_range_rho(rgrho_);
     set_range_ye(rgye_);
     // TODO: first compute temp as a function of rho, ye, and eps, and then initialize its range
@@ -53,11 +27,49 @@ public:
     set_range_temp(rgeps_);
   }
 
-  // destructor
+
+  // Destructor
   //CCTK_HOST CCTK_DEVICE CCTK_ATTRIBUTE_ALWAYS_INLINE inline ~eos_tabulated3d();
 
 
-  // routines
+  // Routine reading the EOS table and filling the corresponding object
+  CCTK_HOST CCTK_DEVICE CCTK_ATTRIBUTE_ALWAYS_INLINE inline void read_eos_table(const string &filename) {
+    auto fapl_id = H5Pcreate(H5P_FILE_ACCESS);
+    assert(fapl_id >= 0);
+    hid_t file_id = 0;
+    int rank_id;
+    CHECK_ERROR(MPI_Comm_rank(MPI_COMM_WORLD, &rank_id));
+
+    #ifdef H5_HAVE_PARALLEL
+    CHECK_ERROR(H5Pset_fapl_mpio(fapl_id, MPI_COMM_WORLD, MPI_INFO_NULL));
+    CHECK_ERROR(H5Pset_all_coll_metadata_ops(fapl_id, true));
+    file_id = H5Fopen(filename.c_str(), H5F_ACC_RDONLY, fapl_id);
+    #else
+    fapl_id = H5P_DEFAULT;
+    if (rank_id == 0) {
+      file_id = H5Fopen(filename.c_str(), H5F_ACC_RDONLY, fapl_id);
+    }
+    // TODO: bcast table info to all ranks
+    #endif
+
+    assert(file_id >= 0);
+
+    // TODO: actually read the EOS table and fill the EOS object
+
+    CHECK_ERROR(H5Pclose(fapl_id));
+
+    #ifdef H5_HAVE_PARALLEL
+    CHECK_ERROR(H5Fclose(file_id));
+    #else
+    if (rank_id == 0) {
+      CHECK_ERROR(H5Fclose(file_id));
+    }
+    #endif
+
+    return;
+  }
+
+
   CCTK_HOST CCTK_DEVICE CCTK_ATTRIBUTE_ALWAYS_INLINE inline CCTK_REAL press_from_valid_rho_eps_ye(const CCTK_REAL rho, const CCTK_REAL eps, const CCTK_REAL ye) const
 {
   CCTK_REAL press = 0.0; //tab3d_press(rho, eps, ye, &ierr);
